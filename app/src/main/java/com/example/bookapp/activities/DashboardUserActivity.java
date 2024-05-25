a53a60aa39b8c24bbcc508c5f88a3d0aed7b46f1
PhanMai1603 <inet.ngocmai.v6@gmail.com>
2024-05-26 04:27:05 +0700

    show book user

--- app/src/main/java/com/example/bookapp/activities/DashboardUserActivity.java
+++ app/src/main/java/com/example/bookapp/activities/DashboardUserActivity.java
@@ -1,17 +1,38 @@
 package com.example.bookapp.activities;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import com.example.bookapp.BooksUserFragment;
 import com.example.bookapp.databinding.ActivityDashboardUserBinding;
+import com.example.bookapp.models.ModelCategory;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
 
 public class DashboardUserActivity extends AppCompatActivity {
 
+    //to show in tabs
+    public ArrayList<ModelCategory> categoryArrayList;
+    public ViewPagerAdapter viewPagerAdapter;
     private ActivityDashboardUserBinding binding;
 
     private FirebaseAuth firebaseAuth;
@@ -24,6 +45,9 @@ public class DashboardUserActivity extends AppCompatActivity {
 
         firebaseAuth = firebaseAuth.getInstance();
         checkUser();
+
+        setupViewPagerAdapter(binding.viewPager);
+        binding.tabLayout.setupWithViewPager(binding.viewPager);
 
         binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +63,104 @@ public class DashboardUserActivity extends AppCompatActivity {
                 startActivity(new Intent(DashboardUserActivity.this, ProfileActivity.class));
             }
         });
+    }
+
+    private void setupViewPagerAdapter(ViewPager viewPager){
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT, this);
+
+        categoryArrayList = new ArrayList<>();
+
+        //load categories from firebase
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Categories"); //be careful of spellings\
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                //clear before adding to list
+                categoryArrayList.clear();
+
+                /*Load Categories - Static e.g. All, Most Viewed, Most Downloaded */
+                //Add data to models
+                ModelCategory modelAll = new ModelCategory("01", "All", " ", 1);
+                ModelCategory modelMostViewed = new ModelCategory("02", "Most Viewed", " ", 1);
+                ModelCategory modelMostDownloaded = new ModelCategory("03", "Most Downloaded", " ", 1);
+                //add data to view pager adapter
+                categoryArrayList.add(modelAll);
+                viewPagerAdapter.addFragment(BooksUserFragment.newInstance(
+                        ""+modelAll.getId(),
+                        ""+modelAll.getCategory(),
+                        ""+modelAll.getUid()
+                ), modelAll.getCategory());
+                viewPagerAdapter.addFragment(BooksUserFragment.newInstance(
+                        ""+modelAll.getId(),
+                        ""+modelAll.getCategory(),
+                        ""+modelAll.getUid()
+                ), modelMostViewed.getCategory());
+                viewPagerAdapter.addFragment(BooksUserFragment.newInstance(
+                        ""+modelAll.getId(),
+                        ""+modelAll.getCategory(),
+                        ""+modelAll.getUid()
+                ), modelMostDownloaded.getCategory());
+                //refresh list
+                viewPagerAdapter.notifyDataSetChanged();
+
+                //Now Load from firebase
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    //get data
+                    ModelCategory model = ds.getValue(ModelCategory.class);
+                    //add data to list
+                    categoryArrayList.add(model);
+                    //add data to viewPagerAdapter
+                    viewPagerAdapter.addFragment(BooksUserFragment.newInstance(
+                            ""+ modelAll.getId(),
+                            ""+ modelAll.getCategory(),
+                            ""+ modelAll.getUid()), model.getCategory());
+                    //refresh list
+                    viewPagerAdapter.notifyDataSetChanged();
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled( DatabaseError error) {
+
+            }
+        });
+
+        //set adapter to view pager
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+
+    public class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private ArrayList<BooksUserFragment> fragmentList = new ArrayList<>();
+        private ArrayList<String> fragmentTitleList = new ArrayList<>();
+        private Context context;
+        public ViewPagerAdapter( FragmentManager fm, int behavior, Context context) {
+            super(fm, behavior);
+            this.context = context;
+        }
+
+        @Override
+        public Fragment getItem(int position){
+            return fragmentList.get(position);
+        }
+        @Override
+        public int getCount(){
+            return fragmentList.size();
+        }
+
+        private void addFragment(BooksUserFragment fragment, String title){
+            //add fragment passed as parameter in fragmentList
+            fragmentList.add(fragment);
+            //add title passed as parameter in fragmentTitleList
+            fragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
     }
     private void checkUser() {
         //get current user
